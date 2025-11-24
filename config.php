0276d3caba78a617ab7ad689f90002280f3613e0
Julio Alessandro G. H. <145719711+Ale130jk@users.noreply.github.com>
2025-11-24 02:54:32 -0500

    Update SOMETHING

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ $doc_root = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']);
 $folder = str_replace($doc_root, '', $script_dir); 
 define('BASE_URL', $protocol . '://' . $host . $folder . '/'); 
 define('BASE_PATH', __DIR__ . '/');
-define('GEMINI_API_KEY', 'AIzaSyAG5i3c80vb-4DFBGZViMhx9irvwUMMgS8');
+define('GEMINI_API_KEY', 'APIKEY');
 date_default_timezone_set('America/Lima');
 ?>
